d784c3f79d130134199257c500c3e7799abf0946
Danny Chee <dannychee@Dannys-MacBook-Air.local>
2025-04-28 19:57:44 -0700

    added wheel selection

---
+++ app/components/WheelSelector.tsx
@@ -0,0 +1,6 @@
+import { StyleSheet, View, Pressable, Text } from 'react-native';
+import { Image, type ImageSource  } from 'expo-image';
+
+const restaurantWheelImage = require('@/assets/');
+
+export default function wheelSelectBox()
